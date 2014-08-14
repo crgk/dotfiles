33640cee12606032fec6402c174b0ca6904dd7f9
Chad Knight <chad.knight@webfilings.com>
2014-08-14 13:41:49 -0500

    add skyreset and skyapi aliases

--- aliases/sky.zsh
+++ aliases/sky.zsh
@@ -8,10 +8,20 @@ mydeploy()
     /usr/local/google_appengine/appcfg.py --oauth2 -A ${1:-wf-richapps} -v update . -V ${2:-chad}
 }
 
+myreset()
+{
+    sky
+    mv tools/bulkdata/accounts.csv tools/bulkdata/accounts.csv.bak
+    cp ../my_accounts.csv tools/bulkdata/accounts.csv
+    python tools/erase_reset_data.py --admin=chad.knight@workiva.com --password=${1:-W3bfilings$}
+    mv tools/bulkdata/accounts.csv.bak tools/bulkdata/accounts.csv    
+}
+
 alias skytest="python manage.py test"
 alias skyserver="python manage.py runserver localhost:8001"
-alias skyreset="python tools/erase_reset_data.py --admin=chad.knight@workiva.com --password=W3bfilings$"
+alias skyreset="myreset"
 alias skyconsole="myconsole"
 alias skydeploy="mydeploy"
+alias skyapi="python tools/remote_api/setup_api_testing.py --api_testing_root /Users/chadknight/workspaces/wf/api-testing"
 
 alias wfconsole="myconsole wf-richapps.appspot.com s~wf-richapps"
