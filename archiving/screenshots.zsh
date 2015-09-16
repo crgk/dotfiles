bab3fdf09b58e11d0d2e518bc3817fb2c7b7bd34
Chad Knight <chad.knight@webfilings.com>
2015-09-16 10:45:26 -0500

    cleanup

--- archiving/screenshots.zsh
+++ archiving/screenshots.zsh
@@ -1,12 +1,14 @@
 #!/bin/zsh
 SCREENSHOTS_DIR=${HOME}/Pictures/Screenshots
 
-today="$(date +%Y.%m.%d)"
-today_dir=${SCREENSHOTS_DIR}/${today}
+yesterday="$(date -v-1d +%Y.%m.%d)"
+yesterday_dir=${SCREENSHOTS_DIR}/archive/${yesterday}
 
-if [ ! -d "$today_dir" ];
+if [ ! -d "$yesterday_dir" ];
     then
         print "Archiving past screenshots...";
-        mkdir -p $today_dir;
-        mv $SCREENSHOTS_DIR/*.png $today_dir;
+        mkdir -p $yesterday_dir;
+        mv $SCREENSHOTS_DIR/*.png $yesterday_dir;
+    else
+        print "Past screenshots already archived: "${yesterday_dir};
 fi
