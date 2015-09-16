053ca00e0170ed52b2535c77041327489d6d7a10
Chad Knight <chad.knight@webfilings.com>
2015-09-16 13:44:03 -0500

    archive.zsh

---
+++ archive.zsh
@@ -0,0 +1,32 @@
+#! /usr/bin/zsh
+
+function archive {
+    if (( $# < 3))
+        then 
+            print "archive: usage: archive target name pattern";
+            return 1;
+    fi
+    emulate -L zsh
+    setopt extendedglob;
+
+    target=$1
+    name=$2
+    pattern=$3
+
+    yesterday="$(date -v-1d +%Y.%m.%d)"
+    yesterday_dir=${target}/archive/${yesterday}
+
+    if [ ! -d "${~pattern}" ];
+        then
+            print "archive: no matches found for $pattern";
+            return 0;
+    fi
+    if [ ! -d "$yesterday_dir" ];
+        then
+            print "archive: archiving past $name...";
+            mkdir -p $yesterday_dir;
+            mv -- ${~pattern} ${yesterday_dir};
+        else
+            print "archive: past $name already archived: $yesterday_dir";
+    fi
+}
